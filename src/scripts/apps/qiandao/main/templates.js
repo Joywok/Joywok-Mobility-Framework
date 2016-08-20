d746f7c154544583840110af3f7e9b9dfc021f8d
zhailei <zhailei2011@gmail.com>
2016-08-20 13:08:51 +0800

    fix all

--- src/scripts/apps/qiandao/main/templates.js
+++ src/scripts/apps/qiandao/main/templates.js
@@ -7,4 +7,11 @@
 Jma.module('Qiandao', function(Qiandao, Jma, Backbone, Marionette, $, _){
 	var Templates = {};
 	Qiandao.Templates =  Templates;
+
+	Templates.LayoutViewTemplate = _.template('<div class="qiandao">\
+																								<div class="qiandao-title">签到列表</div>\
+																								<div class="qiandao-new">新建</div>\
+																								<div class="qiandao-w"></div>\
+																							</div>')
+
 });

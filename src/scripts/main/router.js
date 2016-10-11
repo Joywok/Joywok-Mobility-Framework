673bb1037b05bd1273fa176348e4102626aa42b8
翟磊 <zhailei2011@gmail.com>
2016-10-12 07:21:35 +0800

    去除默认路由

--- src/scripts/main/router.js
+++ src/scripts/main/router.js
@@ -8,13 +8,14 @@ Jma.module('Router', function(Router, Jma, Backbone, Marionette, $, _){
     Router.Router = Jma.AppRouter.extend({
         appRoutes:{
             '':'index',
-            'regist':'index',
+            // 'regist':'index',
         }
     });
     Router.Controller = Marionette.Controller.extend({
         index: function(){
-            Backbone.history.navigate('#regist');
-            Jma.Regist.StartApp();
+            // Backbone.history.navigate('#regist');
+            // Jma.Regist.StartApp();
+          console.log('启动了')
         },
         post: function(){
             console.log(123);

2c101d33922401897f6d5607fd5608ecc6f5d3d4
zhailei <zhailei2011@gmail.com>
2017-05-16 17:10:43 +0800

    完善打包

--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,26 @@ let imagemin = require('gulp-imagemin');
 let pngquant = require('imagemin-pngquant');
 let htmlmin = require('gulp-htmlmin');
 let $ = require('gulp-load-plugins')();
+let useref = require('gulp-useref');
+let uglify = require('gulp-uglify');
+let minifycss = require('gulp-minify-css');
 let express = require('express');
+let gulpif = require('gulp-if');
 let router = express.Router();
 
 gulp.task('styles:sass', ()=>{
   var sass = require('gulp-ruby-sass');
   var concat = require('gulp-concat');
    sass(['src/scss/*.scss'],{
-        style: 'expanded',
-        precision: 10
-        })
-    .on('error', console.error.bind(console))
-    .pipe(gulp.dest('build/styles'))
-    .pipe(gulp.dest('src/styles'))
-    .pipe($.size({title:'build/styles'}));
+      style: 'expanded',
+      precision: 10
+      })
+  .on('error', console.error.bind(console))
+  .pipe(gulp.dest('build/styles'))
+  .pipe(gulp.dest('src/styles'))
+  .pipe($.size({title:'build/styles'}));
 });
 gulp.task('styles', ['styles:sass']);
-gulp.task('html', function() {
-  return gulp.src('src/public/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('build/'));
-});
 gulp.task('mini',function(){
 	return gulp.src('src/images/**/*')
 		.pipe(imagemin({
@@ -41,7 +40,15 @@ gulp.task('concatRoutes',function(){
     .pipe(concat('router.js'))
     .pipe(gulp.dest('routers'))
 })
-gulp.task('default',["html","styles",'concatRoutes'],function(){
+gulp.task('public',()=>{
+  return gulp.src('src/public/index.html')
+    .pipe(useref({searchPath: '.',}))
+    .pipe(gulpif('*.css', minifycss()))
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build/public/'));
+})
+gulp.task('default',["styles",'concatRoutes'],function(){
   gulp.watch(['routers/router/*.js'],['concatRoutes']);
   gulp.watch(['src/public/*.html'],['html']);
   gulp.watch(['src/styles/*.scss'],['styles:sass']);
